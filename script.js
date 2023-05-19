946920bed2b91bf2f091256ebdbfffa50d5cb680
Gabriel Henrique Silva <gabrimiga55@gmail.com>
2023-05-19 16:20:16 -0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Olá mundo!");
+console.log("Gabriel");
+var valor = confirm("Vamos juntos aprender a programar JavaScript");
+if (valor == true) {
+    alert("Parabéns! Vamos lá.");
+} else {
+    alert("Opção errada, aperte F5 para recarregar a página.");
+}
+var email = prompt("Digite seu email");
+alert("O email " + email + " foi coletado com sucesso.");
