f3f582303a3b104e9c6a3a7176e2451440ce328b
Gabriel Henrique Silva <gabrimiga55@gmail.com>
2023-05-24 14:50:39 -0300

    update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-alert("Olá mundo!");
-console.log("Gabriel");
-var valor = confirm("Vamos juntos aprender a programar JavaScript");
-if (valor == true) {
-    alert("Parabéns! Vamos lá.");
+//alert("Olá mundo!");
+//console.log("Gabriel");
+//var valor = confirm("Vamos juntos aprender a programar JavaScript");
+//if (valor == true) {
+//    alert("Parabéns! Vamos lá.");
+//} else {
+//    alert("Opção errada, aperte F5 para recarregar a página.");
+//}
+/*var email = prompt("Digite seu email");
+if (email == null) {
+    alert("O email " + email + " foi coletado com sucesso.");
 } else {
-    alert("Opção errada, aperte F5 para recarregar a página.");
-}
-var email = prompt("Digite seu email");
-alert("O email " + email + " foi coletado com sucesso.");
+    alert("Email não coletado.");
+}*/
+
+var prova = parseFloat(prompt("Qual a nota da prova?"));
+var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+var notafinal = prova + trabalho;
+alert(notafinal);
